43699ba5e2ff2b1c02322f9186c16907c40416dd
Github_cosovo33 <zaougaamine00@gmail.com>
2022-04-27 15:34:57 +0100

    27/04/2022 commit still to fix bugs

--- src/components/registration/index.jsx
+++ src/components/registration/index.jsx
@@ -1,106 +1,101 @@
-import * as React from "react";
-import CssBaseline from "@mui/material/CssBaseline";
-import { Paper } from "@mui/material";
-import { Avatar } from "@mui/material";
-import { FormControl, Button} from "@mui/material"  ;
-import { TextField } from "@mui/material";
-
-const Register =()=> {
- 
-const [user, setUser]=React.useState({
-    userName: "",
-    password: "",
-    passwordConfrim: "",
-  })
-  
-  const Mychange = (e) => {
-    let nam = e.target.name;
-    let val = e.target.value;
-    setUser({ ...user, [nam]: val });
-};
-const strCompare=(p,s)=>{
-    return p===s;
-}
-  const passwordMatch = () => {
-      console.log(user);
-     let v =  strCompare(user.password,user.passwordConfrim) ;
-     console.log(v);
-     /*if(v===false)
-     {alert("password miss match. try again");
-    setUser({...user,[user.passwordConfrim]:''})
-}else console.log(user);*/
-}
+import React, { useState } from "react";
+import {
+  Box,
+  Card,
+  Container,
+  CssBaseline,
+  FormGroup,
+  TextField,
+} from "@mui/material";
+import { Button } from "@mui/material";
 
 
-  const submitRegistration = e => {
-    e.preventDefault();
-    passwordMatch();
-    }
-    
-    return (
-      <div >
-        <CssBaseline />
 
-        <Paper >
-          
-            <FormControl required fullWidth margin="normal">
-              
-              <TextField
-                autoFocus
-                margin="dense"
-                name="username"
-                id="nameSu"
-                label="Email Address"
-                type="email"
-                fullWidth
-                variant="standard"
-                onChange={Mychange}
-                required
-              />
-            </FormControl>
 
-            <FormControl required fullWidth margin="normal">
-            <TextField
-                autoFocus
-                margin="dense"
-                name="password"
-                id="pwd"
-                label="pasword"
-                type="password"
-                fullWidth
-                variant="standard"
-                onChange={Mychange}
-                required
-              />
-            </FormControl>
-
-            <FormControl required fullWidth margin="normal">
-            <TextField
-                autoFocus
-                margin="dense"
-                name="passwordConfirm"
-                id="cpwd"
-                label="pasword"
-                type="password"
-                fullWidth
-                variant="standard"
-                onChange={Mychange}
-                required
-              />
-            </FormControl>
-            <Button
-              fullWidth
-              variant="outlined"
-              
-              type="submit"
-              onClick={submitRegistration}
-            >
-              Join
-            </Button>
-        </Paper>
-      </div>
-    );
+const styles = {
+  paperContainer: {
+      backgroundImage: `url(${"../../../public/leafWP.jpg"})`
   }
+};
+const Register = () => {
 
+  // create state variables for each input
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleClose = () => {
+    setFirstName("");
+    setLastName("");
+    setEmail("");
+    setPassword("");
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(firstName, lastName, email, password); //axios post request to the backend server
+    handleClose();
+  };
+
+  return (
+   
+    <Container styles={styles.paperContainer} sx={{bgcolor:'#ffcdd2',textAlign:'center',width:2/3}} >
+      <CssBaseline>
+        <Card sx={{ margin:'auto',width:2/3}}>
+        <form onSubmit={handleSubmit}>
+          <Box sx={{display:'flex'}}>
+          <TextField
+            sx={{width:1/2}}
+            label="First Name"
+            variant="filled"
+            required
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+          <TextField
+            sx={{width:1/2}}
+            label="Last Name"
+            variant="filled"
+            required
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+</Box>
+         
+          <FormGroup>
+            <TextField
+            fullWidth
+              label="Email"
+              variant="filled"
+              type="email"
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <TextField
+            fullWidth
+              label="Password"
+              variant="filled"
+              type="password"
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </FormGroup>
+          <Box>
+            <Button variant="contained" onClick={handleClose}>
+              Cancel
+            </Button>
+            <Button type="submit" variant="contained" color="primary">
+              Signup
+            </Button>
+          </Box>
+        </form>
+        </Card>
+      </CssBaseline>
+    </Container>
+    
+  );
+};
 
 export default Register;
